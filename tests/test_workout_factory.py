7dc8c6f670a3790125c706616bc649384ca9ea38
elenaMereloMolina <elenamerelo@correo.ugr.es>
2020-09-22 18:03:42 +0200

    test: constructor de workout factory, refs #25

--- tests/test_workout_factory.py
+++ tests/test_workout_factory.py
@@ -11,3 +11,15 @@ def test_read_data_file():
     names = list(map(lambda x: x["name"], array))
     assert names == ['plank', 'squat', 'pushup', 'mountainclimber', 'superman', 'burpee', 'jumpingjack']    
 
+def test_constructor():
+    array = WorkoutFactory(DATA_FILE).exercises
+    assert array
+    
+    names = list(map(lambda x: x.name, array))
+    assert names == ['plank', 'squat', 'pushup', 'mountainclimber', 'superman', 'burpee', 'jumpingjack']    
+
+    intensities = list(map(lambda x : x.intensity, array))
+    assert intensities == [Intensity.HIGH, Intensity.LOW, Intensity.HIGH, Intensity.MEDIUM, Intensity.LOW, Intensity.HIGH, Intensity.MEDIUM]
+
+    muscle_groups = list(map(lambda x : x.muscle_group, array))
+    assert muscle_groups == [MuscleGroup.UPPERBODY, MuscleGroup.LOWERBODY, MuscleGroup.UPPERBODY, MuscleGroup.TOTALBODY, MuscleGroup.UPPERBODY, MuscleGroup.TOTALBODY, MuscleGroup.TOTALBODY]
