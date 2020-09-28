49a270a49464116c3ee95af517d23908b6b52431
Antonio Gamiz <antoniogamiz10@gmail.com>
2020-09-28 16:58:23 +0200

    feat: añade primera aproximacion de la CLI, refs #29

---
+++ src/__main__.py
@@ -0,0 +1,18 @@
+import fire
+
+from src.workout.workout_factory import *
+
+
+def init(intensity: str, muscle_group: str):
+    """
+    Returns a workout
+    :param intensity: intensity
+    :param muscle_group: muscle group
+    :return: workout based on intensity and muscle group
+    """
+    factory = WorkoutFactory("./src/workout_data.yml")
+    return factory.create_workout(intensity, muscle_group, "")
+
+
+if __name__ == "__main__":
+    fire.Fire(init)
