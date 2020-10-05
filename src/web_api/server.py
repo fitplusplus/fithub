25cd489c1a2f9c2e09808cc2cdaf31be3767071a
ElenaMerelo <nenamer@gmail.com>
2020-10-05 18:48:20 +0200

    termina api web, close #35

--- src/web_api/server.py
+++ src/web_api/server.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 from src.workout.workout_factory import *
-
+from src.workout.exercise import *
 app = Flask(__name__)
 
 @app.route('/workout')
 def get_workouts():
-    wf = WorkoutFactory('src/workout/workout_data.yml')
+    wf = WorkoutFactory('src/workout_data.yml')
+    intensity = intensityMapping[request.args.get('intensity')]
+    muscle_group = muscleGroupMapping[request.args.get('musclegroup')]
+    return jsonify({"exercises": wf.create_workout(intensity, muscle_group).exercises.__str__()})
